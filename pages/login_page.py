4a7306ce17c528b4d02994f9ba46e81dac07e8b9
EZLOGIC <shurik311@yandex.ru>
2022-12-29 20:17:00 +0400

    add register new user method

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,6 +2,7 @@ from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .locators import LoginPageLocators
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -17,3 +18,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        registration_email = self.browser.find_element(*LoginPageLocators.EMAIL)
+        registration_email.send_keys(email)
+        registration_password1 = self.browser.find_element(*LoginPageLocators.PASSWORD)
+        registration_password1.send_keys(password)
+        registration_password2 = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        registration_password2.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER)
+        register_button.click()
+        
