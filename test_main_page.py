69206c8631364d0a17bf5211d2de44d402ae1a89
EZLOGIC <shurik311@yandex.ru>
2022-12-29 16:56:23 +0400

    add methods for basket page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,11 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
+"""def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   
     page.open()                      
     page.go_to_login_page()
@@ -14,5 +15,14 @@ def test_guest_can_go_to_login_page(browser):
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_link()"""
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page = BasketPage(browser, link)
+    page.should_not_be_items_in_basket()
+    page.should_be_empty_message()
+    
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 
@@ -38,7 +39,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.should_be_disappeared()"""
+    page.should_be_disappeared()
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -50,6 +51,16 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.go_to_login_page()"""
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page = BasketPage(browser, link)
+    page.should_not_be_items_in_basket()
+    page.should_be_empty_message()
+    
     
     
